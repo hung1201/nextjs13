88b294b748af096e989f2c12cb6226cb58c6f106
hung1201 <hung1201998@gmail.com>
2023-01-30 16:11:47 +0700

    learn nextjs13

---
+++ app/Header.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import React from "react";
+
+function Header() {
+  return (
+    <header className="p-5 bg-slate-300">
+      <Link className="px-2 py-1 bg-white text-blue-500 rounded-lg" href="/">
+        Home
+      </Link>
+      <Link
+        className="px-2 py-1 ml-1 bg-white text-blue-500 rounded-lg"
+        href="/todos"
+      >
+        Todos
+      </Link>
+    </header>
+  );
+}
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="text-red-500">Home Page</div>;
+}
+
+export default Home;
